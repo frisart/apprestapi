923db6c3b1ab7e4e809180fd0675c2ec53d76236
unknown <gerylinux@gmail.com>
2020-07-02 12:27:06 +0700

    berhasil menampilkan semua data mahasiswa

--- controller.js
+++ controller.js
@@ -6,3 +6,17 @@ var connection = require('./koneksi');
 exports.index = function(req, res){
     response.ok("Aplikasi REST API berjalan", res)
 };
+
+// menampilkan data mahasiswa
+exports.tampilkandatamahasiswa = function(req, res){
+    var myquery = "SELECT * FROM mahasiswa";
+
+    connection.query(myquery, function(error, rows, fields){
+        if (error) {
+            connection.log(error);
+        } else {
+            response.ok(rows, res);
+            // console.log(rows);
+        }
+    });
+}; 
