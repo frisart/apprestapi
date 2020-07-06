69e898356eb5f94fc9cc7db7832f8b78456b1cfc
unknown <gerylinux@gmail.com>
2020-07-06 14:54:50 +0700

    install pakage morgan jwt ip

--- controller.js
+++ controller.js
@@ -81,3 +81,16 @@ exports.hapusDataMahasiswa = function(req, res){
     });
 
 };
+
+
+// menampilakn matakuliah group 
+exports.tampilgroupmatakuliah = function(req, res){
+
+    var myquery = "SELECT mahasiswa.id_mahasiswa, mahasiswa.nim, mahasiswa.nama, mahasiswa.jurusan, matakuliah.matakuliah, matakuliah.sks FROM krs JOIN matakuliah JOIN mahasiswa WHERE krs.id_matakuliah = matakuliah.id_matakuliah AND krs.id_mahasiswa = mahasiswa.id_mahasiswa ORDER BY mahasiswa.id_mahasiswa";
+
+    connection.query(myquery, function(error, rows, fields){
+        if(error) throw error;
+        response.okmatakuliah(rows, res);
+        // console.log(rows);
+    })
+};
--- routes.js
+++ routes.js
@@ -20,4 +20,7 @@ module.exports = function(app){
 
     app.route('/hapus')
         .delete(jsonku.hapusDataMahasiswa);
+
+    app.route('/tampilmatakuliah')
+        .get(jsonku.tampilgroupmatakuliah);
 }
