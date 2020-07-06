480a03db139fa92272ac75af2a3a8b04992a8b8d
unknown <gerylinux@gmail.com>
2020-07-06 16:55:51 +0700

    registrasi berhasil

--- middleware/auth.js
+++ middleware/auth.js
@@ -15,7 +15,7 @@ exports.registrasi = function(req, res){
         tanggal_daftar:new Date()
     }
 
-    var query = "SELECT email from ? WHERE ?";
+    var query = "SELECT email FROM ?? WHERE ?? =?";
     var table = ["user", "email", post.email];
 
     query = mysql.format(query, table);
@@ -25,7 +25,7 @@ exports.registrasi = function(req, res){
             console.log(error);
         } else {
             if (rows.length == 0) {
-                var query = "INSERT INTO ?? SET ??";
+                var query = "INSERT INTO ?? SET ?";
                 var table = ["user"];
 
                 query = mysql.format(query,table);
--- server.js
+++ server.js
@@ -1,13 +1,20 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+
+var morgan = require('morgan');
 const app = express();
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
+app.use(morgan('dev'));
 
 // panggil routes
 var routes = require('./routes');
+const { index } = require('./controller');
 routes(app);
+
+// daftarkan menu router dari index
+app.use('/auth', require('./middleware'));
 
 app.listen(3000, () => {
     console.log('Server started on port');
